c764aff4320a5556906fe69509eec06ed92bfabc
GBTC406013UR <GBTC406013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-21 14:22:35 -0400

    exercise201

---
+++ src/main/java/com/example/demo/Exercise201Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise201Application.class, args);
+	}
+
+}
